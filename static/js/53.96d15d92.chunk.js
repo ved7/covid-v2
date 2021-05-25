52411f3ed7bea9220e944a885d39f654fc6f3898
Vedant <you@example.com>
2021-05-25 18:03:49 +0530

    Updates

---
+++ static/js/53.96d15d92.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcovidtracker=this.webpackJsonpcovidtracker||[]).push([[53],{296:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",(function(){return o}));var c=e(20),i=e(16),o=function(){var t=window;t.addEventListener("statusTap",(function(){Object(c.h)((function(){var n=t.innerWidth,e=t.innerHeight,o=document.elementFromPoint(n/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(t){return Object(i.c)(r,t)})).then((function(){Object(c.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.96d15d92.chunk.js.map
